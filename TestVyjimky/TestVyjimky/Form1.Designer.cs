0b507036852b70ae3a1a7557badb9e6d39c7b883
MartinSyrovy <97514843+syrovym05@users.noreply.github.com>
2023-06-07 09:04:06 +0200

    Add files via upload

---
+++ TestVyjimky/TestVyjimky/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TestVyjimky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Location = new System.Drawing.Point(128, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vypočítat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 358);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(364, 518);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestVyjimky/TestVyjimky/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestVyjimky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowIcon = false;
+            this.CenterToScreen();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int soucet = 0;
+            int pocet = 0;
+
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                try
+                {                    
+                    int cislo = Convert.ToInt32(textBox1.Lines[i]);
+                    if (cislo < 0)
+                    {
+                        checked { soucet += cislo; }
+                        pocet++;
+                    }
+                }                
+                catch(FormatException ex)
+                {
+                    MessageBox.Show(ex.Message + " (Zadávej pouze čísla)");
+                }
+                catch(OverflowException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (ArithmeticException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            try
+            {                
+                if (pocet == 0) 
+                {
+                    throw new DivideByZeroException("Nebylo zadáno žádné záporné číslo!");
+                }
+                else
+                {
+                    double prumer = Math.Round(((double)soucet / (double)pocet), 3);
+                    label2.Text = "Průměr všech záporných čísel je: ";
+                    label1.Text = prumer.ToString();
+                }
+                
+            }           
+            catch (ArithmeticException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
